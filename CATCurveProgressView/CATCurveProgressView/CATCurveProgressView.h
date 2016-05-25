3c243ace12ec5cde90a55338892277b25c830651
CatchZeng <891793848@qq.com>
2016-05-25 15:37:26 +0800

    add progress view

---
+++ CATCurveProgressView/CATCurveProgressView/CATCurveProgressView.h
@@ -0,0 +1,13 @@
+//
+//  CATCurveProgressView.h
+//  CATCurveProgressView
+//
+//  Created by catch on 16/5/25.
+//  Copyright © 2016年 catch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CATCurveProgressView : UIView
+
+@end
---
+++ CATCurveProgressView/CATCurveProgressView/CATCurveProgressView.m
@@ -0,0 +1,21 @@
+//
+//  CATCurveProgressView.m
+//  CATCurveProgressView
+//
+//  Created by catch on 16/5/25.
+//  Copyright © 2016年 catch. All rights reserved.
+//
+
+#import "CATCurveProgressView.h"
+
+@implementation CATCurveProgressView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
